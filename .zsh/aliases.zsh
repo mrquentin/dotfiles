49502c8692cb91dc6c589446faf6f9924ffa8348
Quentin Levasseur <32539717+mrquentin@users.noreply.github.com>
2022-12-01 22:08:14 +0000

    Add zsh base config

---
+++ .zsh/aliases.zsh
@@ -0,0 +1,14 @@
+# Alias
+
+alias k="kubectl"
+alias h="helm"
+alias tf="terraform"
+
+alias ls="exa --icons --group-directories-first"
+alias ll="exa --icons --group-directories-first -l"
+
+alias g="goto"
+alias grep="grep --color"
+
+# Alias for dotfiles management
+alias dot="/usr/bin/git --git-dir=$HOME/repos/dotfiles --work-tree=$HOME"
---
+++ .zsh/goto.zsh
@@ -0,0 +1,4 @@
+autoload -Uz compinit
+compinit
+
+source $(brew --prefix)/Cellar/goto/2.0.0/etc/bash_completion.d/goto.sh
